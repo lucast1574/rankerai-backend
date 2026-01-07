9dc9b856813c38ed0caa104c168acef15d876640
Luc4st1574 <n00281417@upn.pe>
2026-01-07 14:43:04 -0500

    add: all module integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,43 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { configurations, configValidationSchema } from './core/config'; // Updated name
+import { configurations, configValidationSchema } from './core/config';
 import { MongoModule } from './core/database/mongo.module';
 import { GraphqlModule } from './core/graphql/graphql.module';
+
+// Feature Modules
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { ProjectsModule } from './modules/projects/projects.module';
+import { DocumentsModule } from './modules/documents/documents.module';
+
+// Shared Modules
+import { EmailModule } from './shared/email/email.module';
+import { GoogleModule } from './shared/google/google.module';
 
 @Module({
     imports: [
+        // Core Infrastructure
         ConfigModule.forRoot({
             isGlobal: true,
             load: configurations,
-            validationSchema: configValidationSchema, // Matches the new export name
+            validationSchema: configValidationSchema,
         }),
         MongoModule,
         GraphqlModule,
+
+        // Shared Utilities (Email and OAuth)
+        EmailModule,
+        GoogleModule,
+
+        // Application Features
         AuthModule,
         UsersModule,
         RolesModule,
         SubscriptionsModule,
+        ProjectsModule,
+        DocumentsModule,
     ],
 })
 export class AppModule { }
